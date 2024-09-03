ddb30aeb0b9335ea8bd3cd184da687c4f974ca8a
M-Nafi <nafi-m@gmx.de>
2024-09-03 21:40:57 +0200

    added new game component and start-screen component. some new imgs for game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StartScreenComponent],
+  imports: [RouterOutlet, CommonModule, StartScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
