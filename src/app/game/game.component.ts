ce6a9b1479f3aaf13098714fcb81525d3d3323fb
M-Nafi <nafi-m@gmx.de>
2024-09-04 10:40:39 +0200

    added new shuffle function with math.random for cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -8,9 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
-  pickCardAnimation = false;
 
+export class GameComponent {
+
+  constructor() {
+    this.game = new Game();
+    this.newGame();
+  }
+
+  pickCardAnimation = false;
+  game: Game;
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
 
   takeCard() {
     this.pickCardAnimation = true;
