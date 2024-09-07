33a4bf8ddd3ef54fdd332b5676f3fd0e3f22c068
Nafi M <nafim@MacBook-Air-von-Nafi.local>
2024-09-08 01:28:42 +0200

    added new dialog-add-player component. added a new add icon for given names for player with angular aterial

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,11 +4,14 @@ import { Game } from '../../models/game';
 import { PlayerComponent } from "../player/player.component";
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, MatDialogModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
@@ -17,7 +20,7 @@ export class GameComponent {
   currentCard: string = '';
   game: Game;
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
     this.game = new Game();
     this.newGame();
   }
@@ -40,5 +43,17 @@ export class GameComponent {
       }, 1000);
     }
   }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      if (result !== undefined) {
+       
+      }
+    });
+  }
+
   
 }
