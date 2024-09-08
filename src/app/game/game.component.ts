f74d16a411280843a63794f94c7ab97509f4aa37
M-Nafi <nafi-m@gmx.de>
2024-09-08 17:34:12 +0200

    function extendet so that the next player ist selected after playing a card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -36,8 +36,10 @@ export class GameComponent {
       this.currentCard = this.game.stack.pop() || '';
       this.pickCardAnimation = true;
       
-      console.log('New card: ' + this.currentCard);
-      console.log('Game is', this.game);
+      // console.log('New card: ' + this.currentCard);
+      // console.log('Game is', this.game);
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
       setTimeout(() => {
         this.game.playedCard.push(this.currentCard);
         this.pickCardAnimation = false;
